d051d7b9ceac721ab104241e4a2fdd95e062cc16
pbiohazard <38513685+pbiohazard@users.noreply.github.com>
2019-11-17 23:27:11 -0300

    Add files via upload

--- jogo.py
+++ jogo.py
@@ -1,171 +1,205 @@
+#GRUPO 
+    #Gustavo Maciel
+    #Leonardo Almeida
+    #Paulo Rodrigues
+    
+from math import inf as infinity
+from random import choice
+import platform
 import time
+from os import system
 
-class Jogo:
-	def __init__(self):
-		self.comecar_jogo()
+HUMAN = -1
+COMP = +1
 
-	def comecar_jogo(self):
-		self.current_state = [['.', '.', '.'],
-								['.', '.', '.'],
-								['.', '.', '.']]
-		self.player_turn = 'X'
-
-	def tabuleiro(self):
-		for i in range(0, 3):
-			for j in range(0, 3):
-				print('{}|'.format(self.current_state[i][j]), end=" ")
-			print()
-		print()
-
-	def movimentoValido(self, px, py):
-		if px < 0 or px > 2 or py < 0 or py > 2:
-			return False
-		elif self.current_state[px][py] != '.':
-			return False
-		else:
-			return True
-
-	def resultado(self):
-		for i in range(0, 3):
-			if(self.current_state[0][i] != '.' and
-				self.current_state[0][i] == self.current_state[1][i] and
-				self.current_state[1][i] == self.current_state[2][i]):
-				return self.current_state[0][i]
-
-		for i in range(0, 3):
-			if(self.current_state[i] == ['X', 'X', 'X']):
-				return 'X'
-			elif(self.current_state[i] == ['O', 'O', 'O']):
-				return 'O'
-
-		if (self.current_state[0][0] != '.' and
-        	self.current_state[0][0] == self.current_state[1][1] and
-        	self.current_state[0][0] == self.current_state[2][2]):
-			return self.current_state[0][0]
-
-		if(self.current_state[0][2] != '.' and
-			self.current_state[0][2] == self.current_state[1][1] and
-			self.current_state[0][2] == self.current_state[2][0]):
-			return self.current_state[0][2]
+board = [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0],]
 
 
 
-		for i in range(0, 3):
-			for j in range(0, 3):
-				if(self.current_state[i][j] == '.'):
-					return None
+def tabuleiro(state, pc, humano):
 
-		return '.'
+    chars = {-1: humano, +1: pc, 0: ' '}
+    str_line = '---------------'
 
 
-
-	def max(self):
-		maxv = -2
-
-		px = None
-		py = None
-
-		result = self.resultado()
-
-		if result == 'X':
-			return(-1, 0, 0)
-		elif result == 'O':
-			return(1, 0, 0)
-		elif result == '.':
-			return(0, 0, 0)
-
-		for i in range(0, 3):
-			for j in range(0, 3):
-				if self.current_state[i][j] == '.':
-
-					self.current_state[i][j] = 'O'
-					(m, min_i, min_j) = self.min()
-
-					if m > maxv:
-						maxv = m
-						px = i
-						py = j
-
-					self.current_state[i][j] = '.'
-		return(maxv, px, py)
+    print('\n' + str_line)
+    for row in state:
+        for cell in row:
+            symbol = chars[cell]
+            print(f'| {symbol} |', end='')
+        print('\n' + str_line)
 
 
-	def min(self):
-		minv = 2
-		qx = None
-		qy = None
 
-		result = self.resultado()
+def vezPC(pc, humano):
+    depth = len(checaVazio(board))
+    if depth == 0 or FimDeJogo(board):
+        return
 
-		if result == 'X':
-			return (-1, 0, 0)
-		elif result == 'O':
-			return (1, 0, 0)
-		elif result == '.':
-			return (0, 0, 0)
+    tabuleiro(board, pc, humano)
 
-		for i in range(0, 3):
-			for j in range(0, 3):
-				if self.current_state[i][j] == '.':
-					self.current_state[i][j] = 'X'
-					(m, max_i, max_j) = self.max()
-					if m < minv:
-						qx = i
-						qy = j
+    if depth == 9:
+        x = choice([0, 1, 2])
+        y = choice([0, 1, 2])
+    else:
+        move = minimax(board, depth, COMP)
+        x, y = move[0], move[1]
 
-					self.current_state[i][j] = '.'
-
-		return (minv, qx, qy) 
+    set_move(x, y, COMP)
+    time.sleep(1)
 
 
-	def jogar(self):
-		while True:
-			self.tabuleiro()
-			self.result = self.resultado()
+def vezHUMANO(pc, humano):
+    depth = len(checaVazio(board))
+    if depth == 0 or FimDeJogo(board):
+        return
+
+    move = -1
+    moves = {
+        1: [0, 0], 2: [0, 1], 3: [0, 2],
+        4: [1, 0], 5: [1, 1], 6: [1, 2],
+        7: [2, 0], 8: [2, 1], 9: [2, 2],
+    }
+
+    tabuleiro(board, pc, humano)
+
+    while move < 1 or move > 9:
+        try:
+            move = int(input('Digite um valor de 1 a 9: '))
+            coord = moves[move]
+            can_move = set_move(coord[0], coord[1], HUMAN)
+
+            if not can_move:
+                print('Opção inválida!')
+                move = -1
+        except (EOFError, KeyboardInterrupt):
+            print('...')
+            exit()
+        except (KeyError, ValueError):
+            print('Opção inválida')
 
 
-			if self.result != None:
-				if self.result == 'X':
-					print("O vencedor foi X!")
-				elif self.result == 'O':
-					print("O vencedor foi O!")
-				elif self.result == '.':
-					print("Empate")
+def evaluate(state):
+    if vitoria(state, COMP):
+        score = +1
+    elif vitoria(state, HUMAN):
+        score = -1
+    else:
+        score = 0
+    return score
 
-				self.comecar_jogo()
-				return
+def vitoria(state, player):
+    win_state = [
+        [state[0][0], state[0][1], state[0][2]],
+        [state[1][0], state[1][1], state[1][2]],
+        [state[2][0], state[2][1], state[2][2]],
+        [state[0][0], state[1][0], state[2][0]],
+        [state[0][1], state[1][1], state[2][1]],
+        [state[0][2], state[1][2], state[2][2]],
+        [state[0][0], state[1][1], state[2][2]],
+        [state[2][0], state[1][1], state[0][2]],
+    ]
+    if [player, player, player] in win_state:
+        return True
+    else:
+        return False
 
 
-			if self.player_turn == 'X':
+def FimDeJogo(state):
+    return vitoria(state, HUMAN) or vitoria(state, COMP)
 
-				while True: 
-					px = int(input('Digite o valor da linha (0 a 2): '))
-					py = int(input('Digite o valor da linha (0 a 2): '))
 
-					(qx, qy) = (px, py)
+def checaVazio(state):
+    cells = []
+    for x, row in enumerate(state):
+        for y, cell in enumerate(row):
+            if cell == 0:
+                cells.append([x, y])
+    return cells
 
-					if self.movimentoValido(px, py):
-						self.current_state[px][py] = 'X'
-						self.player_turn = 'O'
-						break
-					else:
-						print("Movimento inávalido!")
-			else:
-				(m, px, py) = self.max()
-				self.current_state[px][py] = 'O'
-				self.player_turn = 'X'
+def movimento_valido(x, y):
+    if [x, y] in checaVazio(board):
+        return True
+    else:
+        return False
 
+def set_move(x, y, player):
+    if movimento_valido(x, y):
+        board[x][y] = player
+        return True
+    else:
+        return False
+
+
+# Implementação do algoritmo Minimax
+def minimax(state, depth, player):
+  
+    if player == COMP:
+        best = [-1, -1, -infinity]
+    else:
+        best = [-1, -1, +infinity]
+
+    if depth == 0 or FimDeJogo(state):
+        score = evaluate(state)
+        return [-1, -1, score]
+
+    for cell in checaVazio(state):
+        x, y = cell[0], cell[1]
+        state[x][y] = player
+        score = minimax(state, depth - 1, -player)
+        state[x][y] = 0
+        score[0], score[1] = x, y
+
+        if player == COMP:
+            if score[2] > best[2]:
+                best = score  # max value
+        else:
+            if score[2] < best[2]:
+                best = score  # min value
+
+    return best
 
 
 def main():
-	j = Jogo()
-	j.jogar()
+    humano = ''
+    pc = ''
 
-if __name__ == "__main__":
-	main()
+    # Usuário quem escolhe a letra
+    while humano != 'O' and humano != 'X':
+        try:
+            print('')
+            humano = input('Escolha X ou O: ').upper()
+        except (EOFError, KeyboardInterrupt):
+            print('...')
+            exit()
+        except (KeyError, ValueError):
+            print('Escolha da letra')
+
+    # Escolha oposta ao do usuário
+    if humano == 'X':
+        pc = 'O'
+    else:
+        pc = 'X'
+
+    # Jogo em loop
+    while len(checaVazio(board)) > 0 and not FimDeJogo(board):
+    
+        vezHUMANO(pc, humano) 
+        vezPC(pc, humano)
+    
+    if vitoria(board, HUMAN):
+        tabuleiro(board, pc, humano)
+        print('\nVitória humano')
+    elif vitoria(board, COMP):
+        tabuleiro(board, pc, humano)
+        print('\nVitória pc')
+    else:
+        tabuleiro(board, pc, humano)
+        print('\nEmpate! :o')
+    exit()
 
 
-
-
-
-
+if __name__ == '__main__':
+    main()
